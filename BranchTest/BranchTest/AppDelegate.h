1a14821f3584bbe2b73c441f7fa8e876c0e550bc
jotaone <451274909@qq.com>
2016-07-14 12:31:55 +0800

    init

---
+++ BranchTest/BranchTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BranchTest
+//
+//  Created by jota on 16/7/14.
+//  Copyright © 2016年 YY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BranchTest/BranchTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BranchTest
+//
+//  Created by jota on 16/7/14.
+//  Copyright © 2016年 YY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
